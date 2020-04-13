a4495375378d5823f04c8138654e932f752d4dd8
Shershnov Yevhenii <shershnov.yevhenii@pdffiller.team>
2020-04-13 14:34:04 +0300

    [AAD-1279] Add the ability to create a release and a tag

---
+++ src/Services/ReleaseService.php
@@ -0,0 +1,42 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Fluffy\GithubClient\Services;
+
+use Fluffy\GithubClient\Entities\Release;
+use Fluffy\GithubClient\Http\Client as HttpClient;
+
+/**
+ * @package Fluffy\GithubClient\Services
+ */
+class ReleaseService extends AbstractService
+{
+    /**
+     * @param HttpClient $client
+     */
+    public function __construct(HttpClient $client)
+    {
+        parent::__construct($client, '');
+    }
+
+    /**
+     * @param string $owner
+     * @param string $repository
+     * @param string $tagName
+     * @param string $body
+     * @return Release
+     */
+    public function create(string $owner, string $repository, string $tagName, string $body = ''): Release
+    {
+        $response = $this->client->post("/repos/{$owner}{$repository}/releases", [
+            'tag_name' => $tagName,
+            'name' => $tagName,
+            'body' => $body
+        ]);
+
+        $content = \GuzzleHttp\json_decode($response->getBody()->getContents(), true);
+
+        return Release::fromArray($content);
+    }
+}
--- src/Services/TagsService.php
+++ src/Services/TagsService.php
@@ -3,10 +3,8 @@ declare(strict_types=1);
 
 namespace Fluffy\GithubClient\Services;
 
-use Fluffy\GithubClient\Entities\Git\Tree;
 use Fluffy\GithubClient\Entities\Tag;
 use Fluffy\GithubClient\Http\Client as HttpClient;
-use GuzzleHttp\RequestOptions;
 
 /**
  * @package Fluffy\GithubClient\Services
@@ -24,8 +22,7 @@ class TagsService extends AbstractService
     /**
      * @param string $owner
      * @param string $repository
-     * @param string $treeSha
-     * @return Tree
+     * @return Tag[]
      */
     public function all(string $owner, string $repository): array
     {
@@ -34,5 +31,33 @@ class TagsService extends AbstractService
         $content = \GuzzleHttp\json_decode($response->getBody()->getContents(), true);
 
         return Tag::fromCollection($content);
+    }
+
+    /**
+     * @param string $owner
+     * @param string $repository
+     * @param string $targetCommitSHA
+     * @param string $tagName
+     * @param string $tagMessage
+     * @return Tag
+     */
+    public function create(
+        string $owner,
+        string $repository,
+        string $targetCommitSHA,
+        string $tagName,
+        string $tagMessage = ''
+    ): Tag {
+        $response = $this->client->post("/repos/{$owner}/{$repository}/tags", [
+            'tag' => $tagName,
+            'tagMessage' => $tagMessage,
+            'object' => $targetCommitSHA,
+            'type' => 'commit',
+        ]);
+
+        $content = \GuzzleHttp\json_decode($response->getBody()->getContents(), true);
+
+        return Tag::fromArray($content);
+
     }
 }
